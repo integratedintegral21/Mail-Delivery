6b991f0900419bdbad12107eae3726e9fddd735a
Wojciech <w.boncela@gmail.com>
2021-09-07 21:27:11 +0200

    main.py

--- examples/main.py
+++ examples/main.py
@@ -4,14 +4,6 @@ from joblib import load
 from sklearn.utils import shuffle
 from tensorflow.keras.models import load_model
 
-'''
-    TODO:
-    
-    predictor/deliverypredictor/predictor.py modify predict method
-    model saved in models/nn.h5
-    pipeline saved in models/pipeline.pkl
-'''
-
 
 def main():
     model = load_model('../models/nn.h5')
@@ -20,8 +12,10 @@ def main():
     mail_prep_df = pd.read_csv('../data/mail_prep.csv')
     mail_prep_df = shuffle(mail_prep_df)
     sample = mail_prep_df.values[:10, 1:-1]
+    actual_values = mail_prep_df.values[:10, -1]
     predictions = delivery_predictor.predict(sample)
     print(predictions)
+    print(actual_values)
 
 
 if __name__ == "__main__":
