a6f1ae35904e0108879d72f988d97da902b7efba
integratedintegral21 <w.boncela@gmail.com>
2021-09-19 20:00:52 +0100

    replacing locations if not found in maps

--- scraper/google_maps_scraper.py
+++ scraper/google_maps_scraper.py
@@ -17,6 +17,9 @@ OUTPUT_DATA_PATH = 'data_with_distance.csv'
 def get_batch(start_line, batch_size, filename=MAIL_DATA_PATH) -> pd.DataFrame:
     mail_df = pd.read_csv(filename, sep=';').loc[range(start_line, start_line + batch_size)]
     mail_df = mail_df[mail_df['delivery_date'] != 'nie doszedl']
+    # values to be typed in the browser
+    mail_df['search_sending_location'] = mail_df['sending_location']
+    mail_df['search_delivery_location'] = mail_df['delivery_location']
     return mail_df
 
 # section-directions-trip-0
@@ -80,6 +83,34 @@ def get_city_from_delivery_location(location):
 
 
 def add_distance_and_time(batch_df: pd.DataFrame):
+    driver = init_scraper()
+    distances = []
+    durations = []
+    batch_arr = batch_df[['search_sending_location', 'search_delivery_location']].to_numpy()
+    for index in range(len(batch_arr)):
+        locations = batch_arr[index]
+        sending_location = locations[0]
+        delivery_location = locations[1]
+        distance, vehicle_time = get_distance_and_time(sending_location, delivery_location, driver)
+        # try only with city names if exact locations failed
+        if (distance, vehicle_time) == (-1, -1):
+            sending_city = get_city_from_sending_location(locations[0])
+            delivery_city = get_city_from_delivery_location(locations[1])
+            distance, vehicle_time = get_distance_and_time(sending_city, delivery_city, driver)
+            # do not look for these locations again
+            batch_df['search_sending_location'].replace(sending_location, sending_city, inplace=True)
+            batch_df['search_delivery_location'].replace(delivery_location, delivery_city, inplace=True)
+            # update array
+            batch_arr = batch_df[['search_sending_location', 'search_delivery_location']].to_numpy()
+        distances.append(distance)
+        durations.append(vehicle_time)
+    batch_df['distance'] = np.array(distances)
+    batch_df['vehicle_travel_time'] = np.array(durations)
+    driver.close()
+    return batch_df
+
+
+def init_scraper():
     driver_path = '/home/wojciech/chromedriver/chromedriver'
     driver = webdriver.Chrome(driver_path)
     driver.get(
@@ -91,27 +122,17 @@ def add_distance_and_time(batch_df: pd.DataFrame):
     )
     agr_button = agr.find_elements_by_tag_name("button")
     agr_button[1].click()
-    distances = []
-    durations = []
-    for locations in batch_df[['sending_location', 'delivery_location']].values:
-        sending_location = locations[0]
-        delivery_location = locations[1]
-        distance, vehicle_time = get_distance_and_time(sending_location, delivery_location, driver)
-        # try only with city names if exact locations failed
-        if (distance, vehicle_time) == (-1, -1):
-            sending_city = get_city_from_sending_location(locations[0])
-            delivery_city = get_city_from_delivery_location(locations[1])
-            distance, vehicle_time = get_distance_and_time(sending_city, delivery_city, driver)
-        distances.append(distance)
-        durations.append(vehicle_time)
-    batch_df['distance'] = np.array(distances)
-    batch_df['vehicle_travel_time'] = np.array(durations)
-    driver.close()
-    return batch_df
+    return driver
+
+
+def append_batch_to_file(batch):
+    batch.drop(columns=['search_sending_location', 'search_delivery_location'])
+    batch.to_csv(OUTPUT_DATA_PATH, header=False, mode='a', sep=';')
 
 
 def main(beg_line, batch_size):
     batch = get_batch(beg_line, batch_size)
     batch = add_distance_and_time(batch)
     print(batch.to_string())
-    batch.to_csv(OUTPUT_DATA_PATH, header=False, mode='a', sep=';')
+    append_batch_to_file(batch)
+
