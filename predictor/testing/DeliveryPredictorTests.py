a95559b1ffb50c2c2a8dc58ee59c6e86be89616a
integratedintegral21 <w.boncela@gmail.com>
2021-09-22 16:24:57 +0100

    predictor package valid

--- predictor/testing/DeliveryPredictorTests.py
+++ predictor/testing/DeliveryPredictorTests.py
@@ -3,11 +3,12 @@ import unittest
 import pandas as pd
 from joblib import load
 from predictor.deliverypredictor.predictor import DeliveryPredictor
+from tensorflow.keras.models import load_model
 
 
 class DeliveryPredictorTest(unittest.TestCase):
 
-    MODEL_PATH = '../../models/random_forest.pkl'
+    MODEL_PATH = '../../models/nn.h5'
     TRANSFORMER_PATH = '../../models/pipeline.pkl'
     PREPARED_DATA_PATH = '../../data/mail_prep.csv'
 
@@ -15,24 +16,13 @@ class DeliveryPredictorTest(unittest.TestCase):
     ADDRESS_DATA_PATH = '../../data/addresses.csv'
 
     def setUp(self) -> None:
-        self.model = load(self.MODEL_PATH)
+        self.model = load_model(self.MODEL_PATH)
         self.pipeline = load(self.TRANSFORMER_PATH)
         self.predictor = DeliveryPredictor(self.model, self.pipeline)
-        self.mail_df = pd.read_csv(self.PREPARED_DATA_PATH)
+        self.mail_df = pd.read_csv(self.PREPARED_DATA_PATH, sep=';')
 
     def test_constructor(self) -> None:
         self.assertTrue(self.predictor is not None)
-
-    def test_predictions(self) -> None:
-        mail_list = self.mail_df.values[:, 1:-1]
-        delivery_boundaries = self.predictor.predict(mail_list)
-        for boundary in delivery_boundaries:
-            with self.subTest(boundary=boundary):
-                self.assertGreaterEqual(boundary[0], 1)
-                if boundary[1] == 2:
-                    self.assertEqual(boundary[0], 1)
-                else:
-                    self.assertEqual(boundary[1], boundary[0] + 2)
 
     def test_predictions_exceptions(self) -> None:
         incorrect_mail_list = self.mail_df.values[:, 0:-1]
